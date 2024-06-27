3a943cb10deb57b5d6d0dec699c302537a4d6093
Laurence Luo <admin@lzc.app>
2024-06-27 17:56:40 +0800

    Update

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 
-namespace SignalRChat.Hubs
-{
-    public class ChatHub : Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
+namespace SignalRChat.Hubs {
+    public class ChatHub : Hub {
+        public async Task SendMessage(string user, string message) {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
