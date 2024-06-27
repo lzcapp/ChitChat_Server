02479d44961d34d9bf28abef7265eef56d9bc302
Laurence Luo <admin@lzc.app>
2024-06-27 17:49:23 +0800

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChitChat_Server.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() { }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ChitChat_Server
+ 
